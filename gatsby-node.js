4a2a2bd1295b9ea3aa5edb3e9a04147e800e4c58
Jan Paul Fernandez <jpaul.fernandez18@gmail.com>
2021-01-27 15:22:52 +0800

    Update gatsby-node.js
    added netlifycms compatibility

--- gatsby-node.js
+++ gatsby-node.js
@@ -72,44 +72,67 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 }
 
-exports.createSchemaCustomization = ({ actions }) => {
+// exports.createSchemaCustomization = ({ actions }) => {
+//   const { createTypes } = actions
+
+//   // Explicitly define the siteMetadata {} object
+//   // This way those will always be defined even if removed from gatsby-config.js
+
+//   // Also explicitly define the Markdown frontmatter
+//   // This way the "MarkdownRemark" queries will return `null` even when no
+//   // blog posts are stored inside "content/blog" instead of returning an error
+//   createTypes(`
+//     type SiteSiteMetadata {
+//       author: Author
+//       siteUrl: String
+//       social: Social
+//     }
+
+//     type Author {
+//       name: String
+//       summary: String
+//     }
+
+//     type Social {
+//       twitter: String
+//     }
+
+//     type MarkdownRemark implements Node {
+//       frontmatter: Frontmatter
+//       fields: Fields
+//     }
+
+//     type Frontmatter {
+//       title: String
+//       description: String
+//       date: Date @dateformat
+//     }
+
+//     type Fields {
+//       slug: String
+//     }
+//   `)
+// }
+
+exports.createSchemaCustomization = ({ actions, schema }) => {
   const { createTypes } = actions
-
-  // Explicitly define the siteMetadata {} object
-  // This way those will always be defined even if removed from gatsby-config.js
-
-  // Also explicitly define the Markdown frontmatter
-  // This way the "MarkdownRemark" queries will return `null` even when no
-  // blog posts are stored inside "content/blog" instead of returning an error
-  createTypes(`
-    type SiteSiteMetadata {
-      author: Author
-      siteUrl: String
-      social: Social
-    }
-
-    type Author {
-      name: String
-      summary: String
-    }
-
-    type Social {
-      twitter: String
-    }
-
-    type MarkdownRemark implements Node {
-      frontmatter: Frontmatter
-      fields: Fields
-    }
-
-    type Frontmatter {
-      title: String
-      description: String
-      date: Date @dateformat
-    }
-
-    type Fields {
-      slug: String
-    }
-  `)
+  const typeDefs = [
+    "type MarkdownRemark implements Node { frontmatter: Frontmatter }",
+    schema.buildObjectType({
+      name: "Frontmatter",
+      fields: {
+        tags: {
+          type: "[String!]",
+          resolve(source, args, context, info) {
+            const { tags } = source
+            if (source.tags == null || (Array.isArray(tags) && !tags.length)) {
+              return []
+            }
+            return tags
+          },
+        },
+      },
+    }),
+  ]
+  createTypes(typeDefs)
 }
